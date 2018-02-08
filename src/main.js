62c277e89d77607fed7f35114517027936d5c4ef
Simo Martomaa <smartoma@gmail.com>
2018-02-08 12:27:16 +0200

    Increased difficulty from 2 to 4

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ class Block {
 class Blockchain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
+        this.difficulty = 4;
         this.pendingTransactions = [];
         this.miningReward = 100;
     }
